f1d0b1d36b8e11805eb2c959a915bd6c843bc2da
Ljubica <ljukip1441@gmail.com>
2020-07-01 17:54:52 +0200

    #unos_upita azuriran za dodatne provere

--- main.py
+++ main.py
@@ -9,14 +9,21 @@ def load_main_menu():
     print("#################################################################################################################")
     print("Unesite absolutnu putanju do direktorijuma, okucajte TEST da se pokrene sa test direktorijumom ili 0 za izlazak")
     print("#################################################################################################################")
+    control_value = 0
     while True:
         user_input = input(">>>>")
-        if os.path.isdir(user_input):
-            return user_input
-        elif user_input == "TEST":
+        if user_input == "TEST":
             return user_input
         elif user_input == "0":
             exit()
+        elif os.path.isdir(user_input): #proveravamo da li direktorijum sadrzi podirektoriju i validne HTML fajlove
+            for file_name in os.listdir(user_input):
+                tmp_name  = os.path.join(user_input, file_name)
+                if tmp_name.endswith('.html') or os.path.isdir(tmp_name): # direktoriju sadrzi barem jedan poddirektorju ili HTML fajl
+                    control_value = 1
+            if control_value == 1:
+                return user_input
+            break
         else:
             print("Uneliste neispravnu putanju, pokusajte ponovo")
 
